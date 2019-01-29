e88547fa6d5ee74cc412bc5ab66e3cf035e65036
doreta <12480gr@saeinstitute.edu>
2019-01-29 14:18:33 +0200

    new style gyms

--- app/views/gyms/index.php
+++ app/views/gyms/index.php
@@ -3,23 +3,37 @@
         <div class="col-md-6">
             <div class="gymelement">
                 <div class="row">
-                    <div class="col-md-7 offset-md-2">
+                 <!--   <div class="col-md-6 offset-md-2">
                         <p id="gymsnames">Workout Hall Crossfit</p>
                         <img src= " <?php echo URLROOT ?> /images/stars.png" alt="stars"/>
                         <p>Ρόδων 6 & Κυβέλης 17456, Άλιμος</p>
+                        <p><b>Crosfii, Boxing, KingBoxing,Climbing</b></p>
+                        <button class="btnorange" id="btnsearch"><a href="#">Πακέτα Συνδρομών</a></button>
+                    </div>-->
+                    <div class="col-md-2 offset-md-2">
+                         <img id="gymlogo" width="200" src= " <?php echo URLROOT ?> /images/gym/gymlogo.png" alt="logo"/>
+                    </div>
+                    <div class="col-md-6 offset-md-1">
+                        <p id="gymsnames">Workout Hall Crossfit</p>
+                        <img src= " <?php echo URLROOT ?> /images/stars.png" alt="stars"/>
+                        <div id="gymaddress">
+                            <a href="#">
+                             <p>Ρόδων 6 & Κυβέλης 17456, Άλιμος  <img src= " <?php echo URLROOT ?> /images/gym/mappin.png" alt="pin"/></p>
+                            </a>
+                        </div>
                         <p><b>Crosfii, Boxing, KingBoxing,Climbing</b></p>
                         <button class="btnorange" id="btnsearch"><a href="#">Πακέτα Συνδρομών</a></button>
                     </div>
                 </div>
 
             </div>
-
-
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 ">
             <img src= " <?php echo URLROOT ?> /images/gym/bigimggym.jpg" alt="stars">
         </div>
     </div>
+<!-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------WHO WE ARE--------------------------------------------------------------->
 
     <div class="container maintext">
         <div class="row">
@@ -37,27 +51,98 @@
             </div>
         </div>
     </div>
+<!-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------***IMAGES**--------------------------------------------------------------->
+
     <div id="images">
         <div class="container">
             <p class="basictitle" id="gymstitle">ΦΩΤΟΓΡΑΦΙΕΣ</p>
             <div class="row">
                 <div class="col-md-12">
                     <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg" width="365" alt="gymimg">
-                    <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg" width="365"  alt="gymimg">
-                    <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg"  width="365" alt="gymimg">
+                    <img src= " <?php echo URLROOT ?> /images/gym/image2.jpg" width="365"  alt="gymimg">
+                    <img src= " <?php echo URLROOT ?> /images/gym/image3.jpg"  width="365" alt="gymimg">
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12">
-                    <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg" width="365" alt="gymimg">
-                    <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg" width="365"  alt="gymimg">
+                <div class="col-md-12" id="imagerow2">
+                    <img src= " <?php echo URLROOT ?> /images/gym/image2.jpg" width="365" alt="gymimg">
+                    <img src= " <?php echo URLROOT ?> /images/gym/image3.jpg" width="365"  alt="gymimg">
                 </div>
             </div>
         </div>
     </div>
+<!-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------***TRAINERS**--------------------------------------------------------------->
+
+    <div class="trainersrow">
+        <div class="container">
+            <p class="basictitle" id="gymstitle">ΠΡΟΣΩΠΙΚΟ</p>
+        </div>   
+            <div class="row">
+                <div class="col-md-1 offset-md-2 " id="arrow">
+                 <img  src= " <?php echo URLROOT ?> /images/back.png" width="40" alt="arrow">
+                </div>
+                <div class="col-md-2" id="trainerphoto">
+                    <img src= " <?php echo URLROOT ?> /images/gym/trainer1.jpg" width="365" alt="gymimg">
+                    <div class="trainerdescr"><p>Γιάννης Κ. - Crossfit trainer</p></div> 
+                </div>
+                <div class="col-md-2 " id="trainerphoto">
+                    <img  src= " <?php echo URLROOT ?> /images/gym/trainer2.jpg" width="365" height="244" alt="gymimg">
+                    <div class="trainerdescr"><p>Μαρία Κ. - Crossfit trainer</p></div> 
+                </div>
+                <div class="col-md-2 "  id="trainerphoto">
+                    <img src= " <?php echo URLROOT ?> /images/gym/trainer3.jpg" width="365" alt="gymimg">
+                    <div class="trainerdescr"><p>Κώστας Κ. - Crossfit trainer</p></div> 
+                </div>
+                <div class="col-md-1  offset-md-1" id="arrow">
+                 <img src= " <?php echo URLROOT ?> /images/front.png" width="40" alt="arrow">
+                </div>
+            </div> 
+       
+    </div>
+
+ <!-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------***SYNDROMES**--------------------------------------------------------------->   
+    <div class="sundromesrow">
+        <div class="container">
+            <p class="basictitle" id="gymstitle">ΠΑΚΕΤΑ ΣΥΝΔΡΟΜΩΝ</p>
+            
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="flex-container-sundromes">
+                        <div class="orangebox">
+                            <p id="month"><b>1 ΜΗΝΑΣ</b></p>
+                            <p><b>CrossFit</b></p>
+                            <p><b>Climbing</b></p>
+                            <p id="price"><b>70€</b></p>
+                        </div>
+                        <div class="orangebox">
+                            <p id="month"><b>3 ΜΗΝΑΣ</b></p>
+                            <p><b>CrossFit</b></p>
+                            <p><b>Climbing</b></p>
+                            <p id="price"><b>180€</b></p>
+                        </div>
+                        <div class="orangebox">
+                            <p id="month"><b>6 ΜΗΝΑΣ</b></p>
+                            <p><b>CrossFit</b></p>
+                            <p><b>Climbing</b></p>
+                            <p id="price"><b>360€</b></p>
+                        </div>
+                        <div class="orangebox">
+                            <p id="month"><b>1 ΕΤΟΣ</b></p>
+                            <p><b>CrossFit</b></p>
+                            <p><b>Climbing</b></p>
+                            <p id="price"><b>450€</b></p>
+                        </div>
+                   </div>    
+                  </div>
+            </div>    
+        </div>
+    </div>
     <div class="row">
-        <div class="col-md-3">
-            <img src= " <?php echo URLROOT ?> /images/gym/image1.jpg" width="365" alt="gymimg">
+        <div class="col-md-12">
+             <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d12592.370471965784!2d23.74543255!3d37.9048978!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sel!2sgr!4v1548759265208" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
 <?php require  APPROOT . '/views/inc/footer.php'?>
