f20a4fa50aabc3234eaa1c8cf5a72636a3e7c46a
doreta <12480gr@saeinstitute.edu>
2019-02-05 16:59:25 +0200

    responcive index

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -3,7 +3,7 @@
         <!-- Grid row -->
      <div class="footerrow"></div>   
         <div class="row">
-            <div class="col-md-2 offset-md-1 col-sm-5 offset-sm-3 mb-4">
+            <div class="col-lg-2 offset-lg-1 col-md-3 offset-md-1 col-sm-5 offset-sm-3 mb-4">
                 <div class="pl-4">
                     <a href="<?php echo URLROOT; ?>"><img class="footer_logo" src="<?php echo URLROOT ?>/images/logo.png" alt="logo_footer"></a>
                     <p id="gymaound">Gymaround</p>
@@ -23,29 +23,30 @@
                 <p><a href="">Πολιτική Cookies</a></p>
                 <p><a href="">FAQ</a></p>
             </div>
-            <div class="col-md-2 col-sm-12 mb-4">
+            <div class="col-lg-2 col-md-4 col-sm-12 mb-4">
                 <h3 class="mb-4" id="footertitle">Συνεργάτες</h3>
                 <div class="row">
                     <div class="col-md-12 offset-md-0 col-sm-6 offset-sm-3">
-                         <a class="btn btn-outline-warning" id="btnfooter" href="<?php echo URLROOT; ?>/owners/index"> Γίνε συνεργάτης   ></a>
+                         <a class="btn btn-outline-warning" id="btnfooter" href="<?php echo URLROOT; ?>/owners/index"> Γίνε συνεργάτης</a>
                    </div>
                 </div>
             </div>
-            <div class="col-md-2 col-sm-12 col-12 mb-4">
+            <div class="col-lg-2 col-md-3 col-sm-12 col-12 mb-4">
                 <h3 class="mb-4" id="footertitle">Βρείτε μας</h3>
-                <div class="row">
-                    <div class="col-md-2 offset-md-0 col-sm-1 offset-sm-3 col-2 offset-3" id="sicon">
-                        <a href="<?php echo URLROOT; ?>"><img class="fblogo" width="18" height="35" src="<?php echo URLROOT ?>/images/fbicon.png" alt="logo_footer"></a>
-                    </div>
-                    <div class="col-md-3 col-sm-1 offset-sm-1 col-2"  id="sicon">
-                        <a href="<?php echo URLROOT; ?>"><img class="fblogo"  src="<?php echo URLROOT ?>/images/instaicon.png" alt="logo_footer"></a>
-                    </div>  
-                    <div class="col-md-1 col-sm-1 offset-sm-1 col-2 " id="sicon">
-                        <a href="<?php echo URLROOT; ?>"><img class="fblogo" src="<?php echo URLROOT ?>/images/twiticon.png" alt="logo_footer"></a>
-                    </div>
-                  </div> 
+                <div class="socialm">
+                    <div class="row">
+                        <div class="col-md-2 offset-md-0 col-sm-1 offset-sm-3 col-2 offset-3" id="sicon">
+                            <a href="<?php echo URLROOT; ?>"><img class="fblogo" width="18" height="35" src="<?php echo URLROOT ?>/images/fbicon.png" alt="logo_footer"></a>
+                        </div>
+                        <div class="col-md-3 col-sm-1 offset-sm-1 col-2"  id="sicon">
+                            <a href="<?php echo URLROOT; ?>"><img class="fblogo"  src="<?php echo URLROOT ?>/images/instaicon.png" alt="logo_footer"></a>
+                        </div>  
+                        <div class="col-md-1 col-sm-1 offset-sm-1 col-2 " id="sicon">
+                            <a href="<?php echo URLROOT; ?>"><img class="fblogo" src="<?php echo URLROOT ?>/images/twiticon.png" alt="logo_footer"></a>
+                        </div>
+                    </div> 
+                </div>
             </div>
-
 
        </div>
         </div>
