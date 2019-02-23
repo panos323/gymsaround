99134c7361764cb2d561574120c19b2db691f0b3
doreta <12480gr@saeinstitute.edu>
2019-02-23 23:03:38 +0200

    gym css

--- app/views/gyms/index.php
+++ app/views/gyms/index.php
@@ -2,7 +2,6 @@
 
     <div class="row">
         <div class="col-lg-6 col-md-12 gymelement">
-          
                 <div class="row">
                  <!-- <div class="col-md-6 offset-md-2">
                         <p id="gymsnames">Workout Hall Crossfit</p>
@@ -11,10 +10,10 @@
                         <p><b>Crosfii, Boxing, KingBoxing,Climbing</b></p>
                         <button class="btnorange" id="btnsearch"><a href="#">Πακέτα Συνδρομών</a></button>
                     </div>-->
-                    <div class="col-lg-3 offset-lg-2 col-md-3 offset-md-1 col-sm-3 offset-sm-1 col-6 offset-1">
+                    <div class="col-lg-3 offset-lg-2 col-md-3 offset-md-1 col-sm-3 offset-sm-1 col-6 offset-1 mt-5">
                          <img id="gymlogo" width="100%" src= " <?php echo URLROOT;?>/images/gym/gymlogo.png" alt="logo"/>
                     </div>
-                    <div class="col-lg-6 offset-lg-1 col-md-6 offset-md-1 col-sm-6 offset-sm-1 col-10 offset-1" >
+                    <div class="col-lg-6 offset-lg-1 col-md-6 offset-md-1 col-sm-6 offset-sm-1 col-10 offset-1 mt-5" >
                         <p id="gymsnames"><?php echo $data['gym']->gym_name; ?></p>
                         <img src= " <?php echo URLROOT;?>/images/stars.png" alt="stars"/>
                         <div id="gymaddress">
@@ -25,8 +24,6 @@
                         <p id="activities"><b>Crosfii | Boxing | KingBoxing | Climbing</b></p>
                        <a class="btnorange" id="btnsundromes" href="#">Πακέτα Συνδρομών</a>
                     </div>
-            
-
             </div>
         </div>
         <div class="col-lg-6 " id="biggymimg">
