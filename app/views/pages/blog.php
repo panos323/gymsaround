fd206f3e0a8b9091de29d72fbfea733d1ada9f0b
doreta <12480gr@saeinstitute.edu>
2019-02-23 20:15:21 +0200

    new style

--- app/views/pages/blog.php
+++ app/views/pages/blog.php
@@ -24,10 +24,10 @@
     <?php endforeach; ?>
 
     <div class="row">
-        <div class="col-md-6 ">
+        <div class="col-md-6 anime">
              <a href="#"><img src= " <?php echo URLROOT;?>/images/blog/imgblog5.jpg"  style="margin-bottom:15px;" width="100%" alt="stars"/></a>
         </div>   
-        <div class="col-md-6 blogbox">
+        <div class="col-md-6 blogbox anime-left">
             <a href="#"> <h4>10 λόγοι για να ξεκινήσετε το τρέξιμο σήμερα</h4></a>
             <br>
             <p>Λάβετε θέσεις, έτοιμοι, πάμε! Αν το γυμναστήριο, σας πνίγει, σας προκαλεί πλήξη και σας βγάζει έξω από το μηνιαίο σας budget,
@@ -36,10 +36,10 @@
     </div>
 
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6  anime">
             <a href="#"> <img src= " <?php echo URLROOT;?>/images/blog/imgblog2.jpeg" style="margin-bottom:15px;" width="100%" alt="stars"/></a>
         </div>   
-        <div class="col-md-6 blogbox">
+        <div class="col-md-6 blogbox anime-left">
             <a href="#"> <h4>Πορτοκάλια και λεμόνια μειώνουν τις συνέπειες της παχυσαρκίας</h4></a>
             <br>
             <p>Tα εσπεριδοειδή δεν είναι πλούσια μόνο σε βιταμίνη C αλλά και σε τρεις άλλες ουσίες οι οποίες μπορεί να
@@ -64,5 +64,78 @@
 
 
 </div>
+
+<script type="text/javascript">
+
+debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+(function(){
+	var $target = $('.anime'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+
+(function(){
+	var $target = $('.anime-left'),
+			animationClass = 'anime-end',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+</script>
 
 <?php require  APPROOT . '/views/inc/footer.php'?>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -93,7 +93,7 @@
 
                                         <div class="col-lg-6 offset-lg-2 col-md-6 offset-md-1" id="bestgymimg" >
                                             <!--start first gym photo to display -->
-                                            <div class="displayFirstGymPhoto anime">
+                                            <div class="displayFirstGymPhoto anime-left">
                                                 <img src="<?php echo URLROOT; ?>/images/bestgym1.jpg" height="auto" class="d-block w-100" alt="Gym Photo">      
                                             </div>
                                             <!--start first gym photo to display -->
@@ -186,8 +186,8 @@
             <div class="col-lg-5 col-md-6">
                 <div class="blogarticle">
                     <p class="rowtitle" id="blogtitle">BLOG</p>
-                    <p  id="articletitle">5 Συμβουλές Διατροφής!</p>
-                    <p  id="article">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <p class="anime-left" id="articletitle">5 Συμβουλές Διατροφής!</p>
+                    <p class="anime-left" id="article">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
                         unknown printer took a galley of type and scrambled 
                         it to make a type specimen book.  </p>
@@ -334,6 +334,34 @@ debounce = function(func, wait, immediate) {
 		animeScroll();
 	}, 200));
 })();
+
+
+
+(function(){
+	var $target = $('.anime-left'),
+			animationClass = 'anime-end',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
 
 </script>
 
